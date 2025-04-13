9ab7a24c1ee903fb67bfe5e523f24a9c3f3c6745
Jenithpaul <70511423+Jenithpaul@users.noreply.github.com>
2025-04-13 23:51:11 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -9,15 +9,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyD-N-ls6j2RqlaPgDUv2vGBrt4CbQ56h0s",
-  authDomain: "belviano-2045e.firebaseapp.com",
-  projectId: "belviano-2045e",
-  storageBucket: "belviano-2045e.firebasestorage.app",
-  messagingSenderId: "808389066006",
-  appId: "1:808389066006:web:938b7fc365bc25c6f52437",
 
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
